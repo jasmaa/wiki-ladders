4067827f9a53c09f1ed026ed16962b8193c22d72
jasmaa <jasonmaa3955@gmail.com>
2019-04-07 22:31:43 -0400

    Add article summaries, make UI nicer

--- game/static/game/game.js
+++ game/static/game/game.js
@@ -1,10 +1,42 @@
 
+var links;
+var ladderPath = [curr];
+
+const MAX_SUMMARY_WORDS = 100;
+const MAX_LINKS = 9999;
+
 // Sets goal header
-function setGoalHeader(){
-    $('#goal-header').html(curr + " → " + end);
+function setHeaders(){
+    $('#goal-header').html(end);
+    $('#curr-header').html(curr);
 }
 
-// jump to page
+// Gets page summary
+function setPageSummary(target, pageId){
+    $.ajax({
+        type: "GET",
+        url: `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${target}`,
+        async: true,
+        dataType: 'jsonp',
+        crossDomain: true,
+        success: function(data) {
+            var pages = data['query']['pages'];
+            for(k in pages){
+
+                var words = pages[k]['extract'].split(" ");
+                var res = words.slice(0, MAX_SUMMARY_WORDS).join(" ");
+                if(words.length > MAX_SUMMARY_WORDS){
+                    res += "...";
+                }
+
+                $(`#${pageId}`).html(res);
+                break;
+            }
+        }
+    });
+}
+
+// Jumps to target page
 function jumpPage(target){
     // query wikipedia api
     $.ajax({
@@ -19,7 +51,7 @@ function jumpPage(target){
 
             // update page
             $('#ladder-list').empty()
-            links.forEach(element => {
+            links.slice(0, 10).forEach(element => {
                 var linkBtn = document.createElement("button");
                 linkBtn.innerHTML = element;
                 linkBtn.className = "btn btn-primary";
@@ -28,10 +60,16 @@ function jumpPage(target){
                 };
 
                 $('#ladder-list').append(linkBtn);
-                $('#ladder-list').append('<br>');
             });
 
-            setGoalHeader();
+            // update page content
+
+            setPageSummary(target, 'curr-content');
+
+            $('#curr-content').html();
+
+            // update headers
+            setHeaders();
             ladderPath.push(curr);
         }
     });
@@ -40,4 +78,5 @@ function jumpPage(target){
 // Start game
 $(document).ready(function(){
     jumpPage(curr);
+    setPageSummary(end, 'goal-content');
 });
